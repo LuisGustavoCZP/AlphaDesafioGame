1c6c85047ab763dc967aee1244145221bd44ef65
JLucas00 <91480284+JLucas00@users.noreply.github.com>
2022-02-15 20:11:27 -0300

    up pastas do front

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+const { NOTIMP } = require("dns");
+
+module.exports = {
+   apps: [
+      {
+         script: "backend/index.js",
+         cwd: "./backend/",
+         name: "Backend",
+         watch: true
+      },
+      {
+         script: "frontend/server.js",
+         cwd: "./frontend/",
+         name: "Frontend",
+         watch: true
+      }
+   ]
+}
---
+++ frontend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+
+
+app.use('/aplicacao1', express.static('./src/aplicacao1'));
+app.use('/aplicacao2', express.static('./src/aplicacao2'));
+app.use('/aplicacao3',express.static('./src/aplicacao3'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
