814fb094446cd9b3bb640ee791adb513afcd8e3c
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-02-16 16:54:14 -0300

    m

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,36 @@ const path = __dirname;
 
 const express = require('express');
 const cors = require('cors');
+var cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-const database = require("scripts/database.js")
+app.use('/test', express.static('./testback'));
+
+const database = require("./scripts/database.js");
+const users = require("./scripts/users.js");
 
 app.get("/", (req, res) => 
 {
     console.log(`Conexão ${req.ip} iniciada...`);
-   
+    res.json(users.users);
 });
 
-app.get("/:fase", (req, res) => 
+app.get("/user", (req, res) => 
+{
+    res.cookie("userData", users.N());
+    res.json({p:"Login Sucess"});
+});
+
+
+app.get("/stage/:stage", (req, res) => 
 {
     console.log(req.params);
-    res.json({markets:markets});
+    res.json({"potions":database.SortPotions(parseInt(req.params["stage"]))});
 });
 
 app.listen(port, () => {console.log(`Servidor iniciado em ${port}`)})
