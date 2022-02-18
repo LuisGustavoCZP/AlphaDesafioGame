6bfeab62770b7f7ce99f1dba3a10e7126d0977e0
JLucas00 <91480284+JLucas00@users.noreply.github.com>
2022-02-18 05:55:14 -0300

    database

---
+++ backend/README.md
@@ -0,0 +1,53 @@
+##README
+
+## Game Project Backend
+
+## DATABASE
+
+* potions.json -> Array em que cada posição representa a fase do jogo EX: potions[0] é a fase mais fácil, e cada posição do array temos outro array com as poçoes, os diretorios dos seus respectivos ícones, e um array contendo o index de seus componentes que estão no banco de dados ingredients.
+
+* ingredients.json -> contém um banco de dados com todos os ingredientes possíveis para fazer as poções do jogo, e os diretorios para seus respectivos ícones
+
+* users.json -> 
+
+## ROUTES
+
+# app.get("/initialpage") 
+-> 
+
+# app.post("/newuser", users.CreateUser)
+-> Requisição do tipo POST para cadastrar um novo usuário no banco de dados.
+
+# app.get("/login", users.RequestUser) 
+-> Requisição do tipo GET para fazer o login do jogo. retornando um objeto {stage: "", slot: "", points: ""} do usuário com o sucesso da requisição.
+
+# app.get("/playgame", users.RequestUser) 
+-> Requisição do tipo GET para requisitar aleatoriamente as poções e itens referentes a elas de acordo com cada fase, a rota deve retornar um array com o nome das poções, ingredientes e diretorios de cada um, no formato: (A Definir)
+[{
+  components: [0, 4, 3],
+  icon: "resources/ingredients/Potion (1).png",
+  potionName: "Poção do Sono"
+},
+{
+  icon: "resources/ingredients/passionfruit.png",
+  name: "Maracujá"
+}, {
+  icon: "resources/ingredients/spiderweb.png",
+  name: "Teia de Aranha"
+}, {
+  icon: "resources/ingredients/sliced-tomato.png",
+  name: "Tomate"
+}]
+
+# app.post("/playgame", users.RequestUser) 
+-> Requisição do tipo POST para alterar os dados de pontuação e fase da base de dados users.json
+
+
+
+## OBS:
+
+
+
+
+
+
--- backend/scripts/database.js
+++ backend/scripts/database.js
@@ -1,49 +1,60 @@
 const fs = require('fs');
 const path = __dirname.replace("scripts", "data/");
 
-const stages = JSON.parse(fs.readFileSync(path+"stages.json"));
-const itens = JSON.parse(fs.readFileSync(path+"ingredients.json"));
-const potions = JSON.parse(fs.readFileSync(path+"potions.json"));5
-const recipes = JSON.parse(fs.readFileSync(path+"recipes.json"));
+//const stages = JSON.parse(fs.readFileSync(path + "stages.json"));
+//const recipes = JSON.parse(fs.readFileSync(path + "recipes.json"));
+const items = JSON.parse(fs.readFileSync(path + "ingredients.json"));
+const potions = JSON.parse(fs.readFileSync(path + "potions.json"));
 
-function RandomSort (_items)
-{
-    const numero = _items.length;
-    return parseInt(Math.random() * numero);
+//potions retorna um array onde cada posição dele contém as poções referentes à um nível 
+//Ex: potions[0] = dados das poções do nível 1
+// potions[1] = dados das poções do nível 2
+// por enquanto só são necessários 3 banco de dados (potions, ingredients, users)
+
+
+function randomSort(_items) {
+   const numero = _items.length;
+   return parseInt(Math.random() * numero);
 }
 
-function SortPotions (_stage)
-{
-    if(_stage >= stages.length) return [];
-    const stage = stages[_stage];
-    const total_pots = [...stage.potions];
-    const sorted_pots = [];
-    for(let j = 0; j < stage.quanty; j++)
-    {
-        const _potionID = RandomSort(total_pots);
-        sorted_pots.push(potions[_potionID]);
-        total_pots.splice(_potionID, 1);
-    }
-    return sorted_pots;
+// sortPotions retorna um array com um objeto da poção sorteada no formato:
+/* [{
+  components: [0, 4, 3],
+  icon: "resources/ingredients/Potion (1).png",
+  potionName: "Poção do Sono"
+}] */
+function sortPotions(_stage) {
+   if (_stage >= potions.length) return [];
+   const num_potions = 1;
+   const total_pots = [...potions[_stage]];
+   const sorted_pots = [];
+   for (let j = 0; j < num_potions; j++) {
+      const potionID = RandomSort(total_pots);
+      sorted_pots.push(total_pots[potionID]);
+      total_pots.splice(potionID, 1);
+   }
+   return sorted_pots;
 }
 
-function SortStage(_stage)
-{
-    const stage = stages[_stage];
-    const _itens = [...itens];
-    const pots = [];
-    const slots = [];
 
-    for(let i = 0; i < _max; i++)
-    {
-        const id = RandomSort(_itens);
-        slots.push(id);
-        _itens.splice(id, 1);
-    }
+// serach components retorna um array com as informações dos ingredientes no formato:
+/* [{
+   icon: "resources/ingredients/passionfruit.png",
+   name: "Maracujá"
+ }, {
+   icon: "resources/ingredients/spiderweb.png",
+   name: "Teia de Aranha"
+ }] */
+function searchComponents(potion) {
+   const potionSelected = potion[0];
+   const componentsId = potionSelected.components;
+   const components = componentsId.map(function (element, index){
+      return items[element]
+   });
 
-    slots.sort((a,b) => {return (Math.random()*2)-1;});
+  
 
-    return {"potion":"", "itens":slots};
+   return components;
 }
 
-module.exports = {itens, RandomSort, SortPotions, SortStage};
+module.exports = { searchComponents, randomSort, sortPotions};
