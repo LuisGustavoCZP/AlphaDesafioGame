1d0100a96ec9ab209f265ce1ee86ff1073fb96cc
httpsbruno <bruno-bbs80@hotmail.com>
2022-02-22 21:56:50 -0300

    game over

--- frontend/src/gameover/script.js
+++ frontend/src/gameover/script.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
-    const userPoints = {name: "Lucas", points: 356, ranking: 5};
+    let userData = {name: "Lucas", points: 356, ranking: 5};
+
+    /*const cookie = document.cookie;
+    Request.get("user", 
+    {params:cookie.replace("userData=", "")},
+    data => 
+    {
+        userData = data;
+        console.log(data);
+    },
+    error => 
+    {
+        console.log("Erro");
+       // window.location.replace(`../menu`);
+    });*/
 
 
-    $("#points").html(`Pontos: ${userPoints.points}`);
-    $("#ranking").html(`Classificação: #${userPoints.ranking}`);
+    $("#points").html(`Pontos: ${userData.points}`);
+    //$("#ranking").html(`Classificação: #${userData.ranking}`);
 
+
+
+    setTimeout(()=>{$(`#gameOverModal`).css("display","block")},2000);
+
+
+    $("btn-restart").on("click", () => {
+         // window.location.replace(`../game`);
+    });
+
+    $("btn-home").on("click", () => {
+         // window.location.replace(`../menu`);
+    });
 
 });
