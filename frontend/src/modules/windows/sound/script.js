fbc8ce44100fd4d83c68f2592853fdffeb45c91f
httpsbruno <bruno-bbs80@hotmail.com>
2022-03-09 22:25:50 -0300

    menu som

--- frontend/src/modules/windows/sound/script.js
+++ frontend/src/modules/windows/sound/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   
-
+    $("#myRange").attr("value", parent.audiosys.volume * 100);
+    $("#sound").html(`VOLUME: ${parent.audiosys.volume * 100}`);
     $(".close").on("click", function() {
-        $(`#Modal`).css("display","none");
+        //$(`#Modal`).css("display","none");
+        parent.modal.src = "";
+        parent.audiosys.play("close");
     });
 
 
     $("#myRange").mousemove( function(){
-        //volume.value = this.value
+        //volume.value = this.
+        parent.audiosys.volume = this.value/100;
         $("#sound").html(`VOLUME: ${this.value}`);
     })
     $("#myRange").change( function(){
-
+        parent.audiosys.volume = this.value/100;
         $("#sound").html(`VOLUME: ${this.value}`);
     });
 
