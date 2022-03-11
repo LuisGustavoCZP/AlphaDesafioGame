7d232548fa470727a9eab91767acbc646a1e461b
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-03-11 14:18:45 -0300

    adicionado modulo do livro (inicial)

--- frontend/src/modules/windows/book/book.js
+++ frontend/src/modules/windows/book/book.js
@@ -1,5 +1,35 @@
 $(document).ready(() => 
 {
+    let recipe = [
+        { "item":{"name": "poção da velocidade", "icon": "assets/potions/1.png"}, "ingredients": [{"name": "Maracujá", "icon": "assets/ingredients/1.png"}, {"name": "Alface", "icon": "assets/ingredients/3.png"}, {"name": "Tomate", "icon": "assets/ingredients/4.png"}] },
+        { "item":{"name": "poção da resistencia", "icon": "assets/potions/2.png"}, "ingredients": [{"name": "Maracujá", "icon": "assets/ingredients/1.png"}, {"name": "Alface", "icon": "assets/ingredients/3.png"}, {"name": "Teia de Aranha", "icon": "assets/ingredients/5.png"}] }
+    ];
+    const totalOfPages = parseInt(recipe.length/2);
+    let thisPage = 1;
+    console.log(recipe[0].ingredients[0].icon);
+    
+    function fillPages(){
+        if(thisPage === 1){
+            fillIngredients(1, 1);
+            fillIngredients(1, 2);
+            fillIngredients(2, 3);
+            fillIngredients(2, 4);
+        }
+    }
+
+    function fillIngredients(page, number){
+        const potionLength = recipe[number-1].ingredients.length;
+        console.log(potionLength);
+        $(`#ing-${number}`).html("");
+        $(`#potion${number}-page${page} h2`).html(recipe[number-1].item.name);
+        $(`#pot-${number}`).attr("src", `../../../images/${recipe[number-1].item.icon}`);
+        for(let i = 0; i < potionLength; i++){
+            $(`#ing-${number}`).append(`<img class="ingredients" src="../../../images/${recipe[number-1].ingredients[i].icon}">`)
+        }
+    }
+
+    fillPages(recipe);
+
     //btn to Play the game
     $("#btn-login").on("click", () => {
         username = $("#username").val();
