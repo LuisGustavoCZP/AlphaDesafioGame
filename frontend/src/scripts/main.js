906ece6c927016ec33a19f7da807b2c3b066cb4f
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-03-14 00:42:56 -0300

    front: spashscreen

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,10 +1,11 @@
 import { User } from "/scripts/user.js";
-const waitTime = 0;
+const waitTime = 10;
 const user = new User ();
 window.gameuser = user;
 
 window.audiosys = document.getElementById("audiosys");
 window.modal = document.getElementById("modal");
+window.modal.src = "modules/splash"
 window.game = document.getElementById("game");
 window.transition = document.getElementById("transition");
 
