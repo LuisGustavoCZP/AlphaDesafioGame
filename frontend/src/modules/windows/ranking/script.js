e93f8c1b93c511108d71bb09e5b62bdaf5dfa76d
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-03-15 16:03:49 -0300

    front: ranking

--- frontend/src/modules/windows/ranking/script.js
+++ frontend/src/modules/windows/ranking/script.js
@@ -3,30 +3,32 @@ $(document).ready(() =>
     $(".menu-background > *").remove();
     
     //const cookie = document.cookie;
-    const ranking = parent.gameuser.ranking;
-
-    let result = `<div id="menu-header">
-                    <div id="title"><strong>CLASSIFICAÇÃO</strong></div>
-                    <span class="close">&times;</span>
-                  </div>
-                 <div id="content">`;
-    ranking.forEach((element,index) => {
-        result += `<div id="place-${index + 1}" class="places">
-                        <p> #${element.classification} - ${element.name}</p>
-                        <p> ${element.highscore} PONTOS</p>
-                    </div>`;
-    });
-    result += `</div>`;
-    console.log(result);
-    
-    $(".menu-background").append(result);
-
-    $(".close").on("click", function() 
+    parent.gameuser.requestRanking((ranking) => 
     {
-        //console.log("Close");
-        parent.modal.src = "modules/windows/main";
-        parent.audiosys.play("close");
-        //$(`#Modal`).css("display","none");
+        let result = `<div id="menu-header">
+                        <div id="title"><strong>CLASSIFICAÇÃO</strong></div>
+                        <span class="close">&times;</span>
+                    </div>
+                    <div id="content">`;
+        ranking.forEach((element, index) => {
+            const id = index + 1;
+            result += `<div id="place-${id}" class="places">
+                            <p> #${id} - ${element.name}</p>
+                            <p> ${element.highscore} PONTOS</p>
+                        </div>`;
+        });
+        result += `</div>`;
+        //console.log(result);
+        
+        $(".menu-background").append(result);
+
+        $(".close").on("click", function() 
+        {
+            //console.log("Close");
+            parent.modal.src = "modules/windows/main";
+            parent.audiosys.play("close");
+            //$(`#Modal`).css("display","none");
+        });
     });
 
 });
