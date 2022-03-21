1604e5e9a80cae60497202762a6a67dc6ef8339b
httpsbruno <bruno-bbs80@hotmail.com>
2022-03-21 20:59:55 -0300

    ranking, game modes modal

---
+++ frontend/src/windows/game-modes/script.js
@@ -0,0 +1,26 @@
+$(document).ready(() => 
+{
+
+    $("#btn-mode-one").on("click", function(){
+        console.log("tela 1");
+        // parent.modal.src = "windows/main"; //window Game 1
+        //parent.audiosys.play("close");
+    });
+
+    $("#btn-mode-two").on("click", function(){
+        console.log("tela 2");
+        //parent.modal.src = "windows/main"; //Window Game 2
+        //parent.audiosys.play("close");
+    });
+
+    
+    $(".close").on("click", function() 
+    {
+        //console.log("Close");
+        parent.modal.src = "windows/main";
+        parent.audiosys.play("close");
+        //$(`#Modal`).css("display","none");
+    });
+
+
+});
