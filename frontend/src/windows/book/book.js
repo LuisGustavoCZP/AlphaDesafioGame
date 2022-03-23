661fe6c3ffe82fc2903ff6e799888ece6f4702c6
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-03-23 10:37:00 -0300

    mudança livro 1

--- frontend/src/windows/book/book.js
+++ frontend/src/windows/book/book.js
@@ -26,30 +26,12 @@ $(document).ready(() =>
 
     fillIngredients();
 
-    //btn to Play the game
-    $("#btn-login").on("click", () => {
-        username = $("#username").val();
-        // password = $("#password").val();
-  
-        if(username.length >= 3 ){
-            //$(`#Modal`).css("display","none");
-            /* callFetchLogin(username); */
-            /* console.log(parent.coiso); */
-            console.log(parent.gameuser.book);
-            
-            //parent.user.login(username);
-        } else {
-            alert("O nome precisa ser mais de 2 caracteres!");
-        }
-    
-    });
-
-    /* $(".close").on("click", function() 
+    $(".close").on("click", function() 
     {
-        console.log("Close");
-        parent.modal.src = "";
-        //$(`#Modal`).css("display","none");
-    }); */
+        parent.modal.src = "windows/main";
+        parent.audiosys.play("close");
+
+    });
 
     /* $("body").on("click", function() 
     {
@@ -81,6 +63,6 @@ $(document).ready(() =>
         }
     }
     
-    $(".this-potion").mouseenter(showInformation);
+    $(".this-potion").on("click",showInformation);
 
 });
