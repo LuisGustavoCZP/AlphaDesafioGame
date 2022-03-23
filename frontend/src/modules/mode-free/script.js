d917cc9f54d6e99f43457faee38d55afdd055251
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-03-23 08:53:21 -0300

    up5

--- frontend/src/modules/mode-free/script.js
+++ frontend/src/modules/mode-free/script.js
@@ -5,8 +5,8 @@ $(document).ready(async function()
     const bookcase = $("#bookcase")[0];
     const cauldron = $("#cauldron")[0];
     const dialogMage = $("#dialog-mage")[0];
-    const btnbook = $("#btn-book")[0];
-    btnbook.onclick = () => {window.modal.src="/windows/book";};
+/*     const btnbook = $("#btn-book")[0];
+    btnbook.onclick = () => {window.modal.src="/windows/book";}; */
     await bookcase.start();
     //if(window.modal) window.modal.src="/windows/stage";
     parent.audiosys.play("open");
