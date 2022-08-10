e3e1e582bb630bd11b16c0abf4d47dfe4a582b65
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-08-10 15:38:57 -0300

    Update script.js

--- frontend/src/modules/mode-free/script.js
+++ frontend/src/modules/mode-free/script.js
@@ -1,115 +1,117 @@
 import { gameTimer, waitFor } from "../../static/scripts/timer.js";
-
-$(document).ready(async function() 
+window.onload(() => 
 {
-    await waitFor(()=>document.getElementById("bookcase"));
-    const bookcase = document.getElementById("bookcase");
-    const cauldron = document.getElementById("cauldron");
-    const dialogMage = document.getElementById("dialog-mage");
-    window.gameuser.requestBook();
-    const btnbook = $("#btn-book")[0];
-    const btnmenu = $("#btn-menu")[0];
-    const btnTips = $("#btn-tips")[0];
-    btnbook.onclick = () => {btnbook.classList.remove("glow"); window.modal.src="/windows/book";};
-    btnmenu.onclick = () => {window.modal.src="/windows/menu";};
-    btnTips.onclick = () => {
-        $("#btn-tips")[0].disabled = true;
-        window.gameuser.requestTip((data)=>{
-        data = [data];
-        data.push({ text:"{f0}", functions:[ () => {$("#btn-tips")[0].disabled = false; } ] })
-        dialogMage.createText(...data)
-    })};
-    await bookcase.start();
-    //if(window.modal) window.modal.src="/windows/stage";
-    parent.audiosys.play("open");
-    cauldron.play(async(ingredients) =>
+    $(document).ready(async function() 
     {
-        if(ingredients && ingredients.length == 2)
+        
+        const bookcase = document.getElementById("bookcase");
+        const cauldron = document.getElementById("cauldron");
+        const dialogMage = document.getElementById("dialog-mage");
+        window.gameuser.requestBook();
+        const btnbook = $("#btn-book")[0];
+        const btnmenu = $("#btn-menu")[0];
+        const btnTips = $("#btn-tips")[0];
+        btnbook.onclick = () => {btnbook.classList.remove("glow"); window.modal.src="/windows/book";};
+        btnmenu.onclick = () => {window.modal.src="/windows/menu";};
+        btnTips.onclick = () => {
+            $("#btn-tips")[0].disabled = true;
+            window.gameuser.requestTip((data)=>{
+            data = [data];
+            data.push({ text:"{f0}", functions:[ () => {$("#btn-tips")[0].disabled = false; } ] })
+            dialogMage.createText(...data)
+        })};
+        await bookcase.start();
+        //if(window.modal) window.modal.src="/windows/stage";
+        parent.audiosys.play("open");
+        cauldron.play(async(ingredients) =>
         {
-            const itens = ingredients;
-            cauldron.reset();
-            console.log(itens, window.gameuser);
-            cauldron.classList.remove("ui-droppable");
-            window.gameuser.sendItems(itens, async (response) => 
+            if(ingredients && ingredients.length == 2)
             {
-                console.log(response);
-                if(response.result)
+                const itens = ingredients;
+                cauldron.reset();
+                console.log(itens, window.gameuser);
+                cauldron.classList.remove("ui-droppable");
+                window.gameuser.sendItems(itens, async (response) => 
                 {
-                    window.modal.src = "";
-                    window.gameuser.lastCreation = response;
-                    window.modal.src = "/windows/newrecipe";
-                } 
-                else
-                {
-                    if(Math.random() > 0.5) 
+                    console.log(response);
+                    if(response.result)
                     {
-                        const resp = await window.gameuser.requestDialog();
-                        await dialogMage.createText(resp);
+                        window.modal.src = "";
+                        window.gameuser.lastCreation = response;
+                        window.modal.src = "/windows/newrecipe";
+                    } 
+                    else
+                    {
+                        if(Math.random() > 0.5) 
+                        {
+                            const resp = await window.gameuser.requestDialog();
+                            await dialogMage.createText(resp);
+                        }
                     }
-                }
-                if(response.status == 1)
-                {
-                    //cauldron.draw(response.result.icon);
-                    btnbook.classList.add("glow");
-                    bookcase.update();
-                    window.gameuser.requestBook();
-                }
-            }); //const response = await 
-            
-            
-            
-            //return true;
-        }
-        //console.log("Dropou objeto", ingredients); 
-    });
+                    if(response.status == 1)
+                    {
+                        //cauldron.draw(response.result.icon);
+                        btnbook.classList.add("glow");
+                        bookcase.update();
+                        window.gameuser.requestBook();
+                    }
+                }); //const response = await 
+                
+                
+                
+                //return true;
+            }
+            //console.log("Dropou objeto", ingredients); 
+        });
 
-    //await 
-    waitFor(async () => 
-    {
-        /* console.log(cauldron.ingredients); */
-        
-        return false;
-    });
-    //await gameTimer(5000);
-
-    let recipe;
-    if(parent.modal){
-        //parent.modal.src="";
-        
-        //recipe = parent.gameuser.currentStage.potion;
-    }
-
-    const dialogs = [
+        //await 
+        waitFor(async () => 
         {
-            text:"VAMOS LÁ {$u}!",
-            click: false,
-            /* time: 1 */
-        },
-        /*{
-            text:"FAÇA \n\t{i0}", 
-            icons:
-            [
-                recipe.item.icon
-            ],
-            time:-1
-        }, */
-    ]
-    await dialogMage.createText(...dialogs);
-    //await parent.gameuser.requestStageStart();
-    
-    /**/
-    /*if(data.status == 0) 
-    {
-        if(data.result) {
-            console.log(`Criou a poção ${data.result}`)
-        }
-        return;
-    }
-        else if (data.status == 1) thisuser.stageTimeout();
-    else {
-        thisuser.stageWin();
-    } */
-    //cauldron.stop();
-    console.log("Coisa coisada!");
-});
+            /* console.log(cauldron.ingredients); */
+            
+            return false;
+        });
+        //await gameTimer(5000);
 
+        let recipe;
+        if(parent.modal){
+            //parent.modal.src="";
+            
+            //recipe = parent.gameuser.currentStage.potion;
+        }
+
+        const dialogs = [
+            {
+                text:"VAMOS LÁ {$u}!",
+                click: false,
+                /* time: 1 */
+            },
+            /*{
+                text:"FAÇA \n\t{i0}", 
+                icons:
+                [
+                    recipe.item.icon
+                ],
+                time:-1
+            }, */
+        ]
+        await dialogMage.createText(...dialogs);
+        //await parent.gameuser.requestStageStart();
+        
+        /**/
+        /*if(data.status == 0) 
+        {
+            if(data.result) {
+                console.log(`Criou a poção ${data.result}`)
+            }
+            return;
+        }
+            else if (data.status == 1) thisuser.stageTimeout();
+        else {
+            thisuser.stageWin();
+        } */
+        //cauldron.stop();
+        console.log("Coisa coisada!");
+    });
+
+});
